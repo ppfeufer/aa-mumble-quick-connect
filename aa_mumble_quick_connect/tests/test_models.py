6b481bd3c83555989e047419eb7c2b76a29736c8
Peter Pfeufer <info@ppfeufer.de>
2025-02-14 09:35:57 +0100

    [ADD] Option to disable link verification
    Suggested by @pvyParts

--- aa_mumble_quick_connect/tests/test_models.py
+++ aa_mumble_quick_connect/tests/test_models.py
@@ -63,7 +63,7 @@ class TestMumbleLink(TestCase):
     @override_settings(MUMBLE_URL="mumble.example.com")
     def test_validate_mumble_url_with_invalid_url(self):
         """
-        Test validate Mumble URL with invalid URL
+        Test validate Mumble URL with invalid URL raises ValidationError
 
         :return:
         :rtype:
@@ -73,6 +73,118 @@ class TestMumbleLink(TestCase):
 
         with self.assertRaisesMessage(
             expected_exception=ValidationError,
-            expected_message=f"The Mumble channel URL must start with '{expected_mumble_base_url}'",
+            expected_message=f'The Mumble channel URL must start with "{expected_mumble_base_url}"',
         ):
-            validate_mumble_url("http://example.com")
+            validate_mumble_url("mumble://example.com")
+
+    @override_settings(MUMBLE_URL="mumble.example.com")
+    def test_validate_mumble_url_with_disabled_url_verification(self):
+        """
+        Test validate Mumble URL with disabled URL verification
+
+        :return:
+        :rtype:
+        """
+
+        self.assertEqual(
+            first=validate_mumble_url(
+                url="mumble://example.com", disable_url_verification=True
+            ),
+            second="mumble://example.com",
+        )
+
+    @override_settings(MUMBLE_URL="mumble.example.com")
+    def test_validate_mumble_url_with_invalid_url_protocol(self):
+        """
+        Test validate Mumble URL with invalid URL protocol raises ValidationError
+
+        :return:
+        :rtype:
+        """
+
+        with self.assertRaisesMessage(
+            expected_exception=ValidationError,
+            expected_message='The Mumble channel URL must start with "mumble://"',
+        ):
+            validate_mumble_url(url="https://example.com")
+
+    @override_settings(MUMBLE_URL="mumble.example.com")
+    def test_validate_mumble_url_with_invalid_url_protocol_and_disabled_url_verification(
+        self,
+    ):
+        """
+        Test validate Mumble URL with invalid URL protocol and disabled URL verification raises ValidationError
+
+        :return:
+        :rtype:
+        """
+
+        with self.assertRaisesMessage(
+            expected_exception=ValidationError,
+            expected_message='The Mumble channel URL must start with "mumble://"',
+        ):
+            validate_mumble_url(
+                url="https://example.com", disable_url_verification=True
+            )
+
+    @override_settings(MUMBLE_URL="mumble.example.com")
+    def test_save_with_valid_url(self):
+        """
+        Test save with valid URL
+
+        :return:
+        :rtype:
+        """
+
+        mumble_link = MumbleLink(
+            name="Foobar",
+            url="mumble://mumble.example.com",
+            section=Section.objects.create(name="Foobar"),
+        )
+        mumble_link.save()
+
+        self.assertEqual(first=mumble_link.url, second="mumble://mumble.example.com")
+
+    @override_settings(MUMBLE_URL="mumble.example.com")
+    def test_save_with_invalid_url_raises_validation_error(self):
+        """
+        Test save with invalid URL raises ValidationError
+
+        :return:
+        :rtype:
+        """
+
+        mumble_link = MumbleLink(
+            name="Foobar",
+            url="mumble://example.com",
+            section=Section.objects.create(name="Foobar"),
+        )
+
+        expected_mumble_base_url = f"mumble://{settings.MUMBLE_URL}"
+
+        with self.assertRaisesMessage(
+            expected_exception=ValidationError,
+            expected_message=f'The Mumble channel URL must start with "{expected_mumble_base_url}"',
+        ):
+            mumble_link.clean()
+
+    @override_settings(MUMBLE_URL="mumble.example.com")
+    def tes_save_with_invalid_url_does_not_raise_validation_error(self):
+        """
+        Test save with invalid URL does not raise ValidationError
+
+        :return:
+        :rtype:
+        """
+
+        mumble_link = MumbleLink(
+            name="Foobar",
+            url="mumble://example.com",
+            section=Section.objects.create(name="Foobar"),
+            disable_url_verification=True,
+        )
+
+        mumble_link.clean()
+        mumble_link.save()
+
+        self.assertEqual(first=mumble_link.url, second="mumble://example.com")
